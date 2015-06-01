a872fea5cbbe5609f4add374ff4c34850be808a7
Graham Swan <thinkswan@gmail.com>
2015-06-01 14:42:26 +0700

    Allow use of process port when starting server
    The app cannot boot on Heroku if the port is hardcoded.

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'));
+
 app.get('/track.gif', function(req, res, next) {
   io.emit('visit', {
     ip: req.ip,
@@ -17,8 +20,6 @@ app.get('/track.gif', function(req, res, next) {
   });
 });
 
-app.use(express.static(__dirname + '/public'));
-
-server.listen(3000);
-
-console.log('Server started at http://localhost:3000');
+server.listen(app.get('port'), function() {
+  console.log('Server is running at http://localhost:' + app.get('port'));
+});
