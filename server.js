85c078259804bc87077ddee42e120c1188ae0e76
Graham Swan <thinkswan@gmail.com>
2015-06-01 12:03:59 +0700

    Add server and client files from tutorial

---
+++ server.js
@@ -0,0 +1,22 @@
+var emptygif = require('emptygif');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.get('/tpx.gif', function(req, res, next) {
+  io.emit('visit', {
+    ip: req.ip,
+    ua: req.headers['user-agent']
+  });
+
+  emptygif.sendEmptyGif(req, res, {
+    'Content-Type': 'image/gif',
+    'Content-Length': emptygif.emptyGifBufferLength,
+    'Cache-Control': 'public, max-age=0'
+  });
+});
+
+app.use(express.static(__dirname + '/public'));
+
+server.listen(3000);
